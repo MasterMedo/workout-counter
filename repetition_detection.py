35ce5a6dde44e343479b102e46390613ab9c4176
MasterMedo <mislav.vuletic@gmail.com>
2021-08-29 17:30:41 +0200

    format repetition detection

--- repetition_detection.py
+++ repetition_detection.py
@@ -1,4 +1,3 @@
-import cv2
 import numpy as np
 import math
 
@@ -8,24 +7,23 @@ average_polar = []
 ALPHA = 0.99
 MAX_BUFFER_SIZE = 100
 
+
 def get_mean_polar_coordinates(A):
     X = np.average(A[..., 0])
     Y = np.average(A[..., 1])
 
-    return (math.sqrt(Y**2 + X**2), math.atan2(Y, X))
+    return (math.sqrt(Y ** 2 + X ** 2), math.atan2(Y, X))
 
-def f2(lastOF, curOF):
-    global div_average, \
-           current_count, \
-           average_polar, \
-           ALPHA, \
-           MAX_BUFFER_SIZE
 
-    div = curOF - lastOF
-    print(curOF, lastOF)
+def f2(optical_flow):
+    global div_average, current_count, average_polar, ALPHA, MAX_BUFFER_SIZE
+
+    # div = curOF - lastOF
+    # print(curOF, lastOF)
+    div = optical_flow
     print(div)
 
-    if (div_average is None):
+    if div_average is None:
         div_average = div
     else:
         div_average += ALPHA * div_average + (1 - ALPHA) * div
@@ -33,14 +31,14 @@ def f2(lastOF, curOF):
     mag, phi = get_mean_polar_coordinates(div_average)
 
     average_polar.append((mag, phi))
-    if (len(average_polar) > MAX_BUFFER_SIZE):
+    if len(average_polar) > MAX_BUFFER_SIZE:
         average_polar = average_polar[1:]
 
     print(average_polar[-5:])
     detection = False
 
     # Mislav's code here:
-    if (detection):
+    if detection:
         current_count += 1
 
-    return (current_count // 2)
+    return current_count // 2
