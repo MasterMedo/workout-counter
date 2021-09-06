d6931a275be4aeaab237309a5f64f22ab337f582
MasterMedo <mislav.vuletic@gmail.com>
2021-09-06 07:54:28 +0200

    version 0.1 - create a dynamical algorithm that works

--- analyse.py
+++ analyse.py
@@ -4,13 +4,14 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 
+from copy import deepcopy
 from itertools import starmap
 from scipy.signal import savgol_filter, find_peaks, peak_prominences
 from scipy.ndimage.filters import uniform_filter1d
 
 from body_part_detection import BodyPart
 from pixel_meter_ratio import pixel_meter_ratio as pmr
-from constants import DEFAULT_FPS as DEFAULT_FPS
+from constants import DEFAULT_FPS, MINIMAL_CONFIDENCE
 
 
 def get_derivation(X):
@@ -30,59 +31,109 @@ def smoothen2(X, fps=DEFAULT_FPS):
     return uniform_filter1d(X, size=size)
 
 
-# if __name__ == "__main__":
-for workout in os.listdir("./workout_data/"):
+def draw(workout="", body_part=""):
+    plt.title(
+        f"{workout} using {body_part} as a point",
+        fontsize=18,
+    )
+    plt.xlabel("frame number")
+    plt.ylabel("coordinate position")
+    plt.legend()
+    plt.show()
+
+
+def is_relevant_body_part(x, minimal_prominence):
+    x = smoothen(x)
+    peaksX = find_peaks(x)[0]
+    prominencesX = peak_prominences(x, peaksX)[0]
+    peaksX = [
+        peak
+        for peak, prominence in zip(peaksX, prominencesX)
+        if prominence > minimal_prominence
+    ]
+    return len(peaksX) > 3
+
+
+if __name__ == "__main__":
+    # for workout in os.listdir("./workout_data/"):
     # workout = "single-arm-dumbbell-triceps-extension.py"
-    # workout = "dumbbell-bicep-curl.py"
+    workout = "dumbbell-bicep-curl.py"
     workout = workout[:-3]
     print(workout)
 
     with open(f"./workout_data/{workout}.py") as f:
         data = [eval(row) for row in f if row.strip()]
 
-    ratio = pmr(data[len(data) // 2])
+    alpha = 0.6
 
-    # 1 / x / ratio -> 1 / x of a meter
-    minimal_prominence = 1 / 4 / ratio
+    body_parts_history = data[0]
+    data_history = []
+    peak_history = [[[0, 0] for _ in range(2)] for _ in range(17)]
+    count_history = [[0, 0] for _ in range(17)]
+    for body_parts in data:
+        # 1 / x / ratio -> 1 / x of a meter
+        ratio = pmr(body_parts)
 
-    # transform matrix 540x17x3 to 3x17x540
+        # motion difference between starting and ending point in an exercise
+        minimal_prominence = 1 / 4 / ratio
+
+        # maximal movement difference between two frames
+        maximal_movement = 1 / 8 / ratio
+
+        for body_part in map(BodyPart, range(17)):
+            x, y, c = body_parts[body_part]
+            x_, y_, c_ = body_parts_history[body_part]
+
+            # if confidence dropped by more than 20% from last frame
+            # and the body part moved more than 12cm, don't move.
+            # this should be dynamic (the less confident the less movement)
+            if c + 0.2 < c_:
+                if abs(x - x_) > maximal_movement:
+                    x = x_
+
+                if abs(y - y_) > maximal_movement:
+                    y = y_
+
+            # smoothing
+            body_parts_history[body_part] = (
+                x_ * alpha + (1 - alpha) * x,
+                y_ * alpha + (1 - alpha) * y,
+                c_ * alpha + (1 - alpha) * c,
+            )
+
+            if c < MINIMAL_CONFIDENCE:
+                continue
+
+            x, y, c = body_parts_history[body_part]
+            xmax, xmin = peak_history[body_part][0]
+            ymax, ymin = peak_history[body_part][1]
+            peak_history[body_part] = [
+                [max(x, xmax), min(x, xmin)],
+                [max(y, ymax), min(y, ymin)],
+            ]
+
+            if x > xmin + minimal_prominence:
+                peak_history[body_part][0][1] = x
+                count_history[body_part][0] += 1
+            if y > ymin + minimal_prominence:
+                peak_history[body_part][1][1] = y
+                count_history[body_part][1] += 1
+            if x < xmax - minimal_prominence:
+                peak_history[body_part][0][0] = x
+                count_history[body_part][0] += 1
+            if y < ymax - minimal_prominence:
+                peak_history[body_part][1][0] = y
+                count_history[body_part][1] += 1
+
+        data_history.append(deepcopy(body_parts_history))
+
     X, Y, C = map(list, starmap(zip, zip(*starmap(zip, data))))
+    X_, Y_, C_ = map(list, starmap(zip, zip(*starmap(zip, data_history))))
     F = list(range(len(X[0])))
 
-    for body_part in map(BodyPart, range(17)):
-        # body_part = BodyPart.LEFT_WRIST
-        x = X[body_part]
-        y = Y[body_part]
-        x_smooth = smoothen(x)
-        y_smooth = smoothen(y)
-        peaksX = find_peaks(x)[0]
-        peaksY = find_peaks(y)[0]
-        prominencesX = peak_prominences(x, peaksX)[0]
-        prominencesY = peak_prominences(y, peaksY)[0]
-        peaksX = [
-            peak
-            for peak, prominence in zip(peaksX, prominencesX)
-            if prominence > minimal_prominence
-        ]
-        peaksY = [
-            peak
-            for peak, prominence in zip(peaksY, prominencesY)
-            if prominence > minimal_prominence
-        ]
-        s = ""
-        if len(peaksX) > 3:
-            s += f"\n    X: {len(peaksX)}"
-        if len(peaksY) > 3:
-            s += f"\n    Y: {len(peaksY)}"
-        if s:
-            print(f"  {body_part.name}{s}")
-
-        # plt.plot(F, y, label="raw")
-        # plt.plot(F, smoothen(y), label="savitsky-golayski")
-        # plt.plot(F, smoothen2(y), label="moving average")
-        # plt.title(f"{workout} using {body_part.name.lower()} as a point")
-        # plt.xlabel("frame number")
-        # plt.ylabel("coordinate position")
-        # plt.legend()
-        # plt.show()
-        # exit()
+    y = Y[BodyPart.LEFT_WRIST]
+    y_ = Y_[BodyPart.LEFT_WRIST]
+    print(count_history)
+    plt.plot(F, y, label="raw data")
+    plt.plot(F, y_, label="processed data")
+    draw()
--- app.py
+++ app.py
@@ -7,22 +7,86 @@ from itertools import count
 from pixel_meter_ratio import pixel_meter_ratio as pmr
 from body_part_detection import detect_body_parts
 
+from body_part_detection import BodyPart
 from constants import MINIMAL_CONFIDENCE, MOVENET_PIXEL_SIZE
+
+
+alpha = 0.6
 
 
 def main(input_path: str, output_file: TextIO) -> None:
     # read the video from the camera
-    cap = cv.VideoCapture(input_path)
+    cap = cv.VideoCapture(0)
 
     # press 'q' to exit the video
+    _, frame = cap.read()
+    body_parts_prev = detect_body_parts(frame, MOVENET_PIXEL_SIZE)
+    peaks = [[[0, 0] for _ in range(2)] for _ in range(17)]
+    counts = [[0, 0] for _ in range(17)]
+
     for i in count():
         is_valid, frame = cap.read()
         if not is_valid or cv.waitKey(1) == ord("q"):
             break
 
         body_parts = detect_body_parts(frame, MOVENET_PIXEL_SIZE)
-        save(output_file, body_parts)
+        # save(output_file, body_parts)
         # draw_screen(frame, body_parts)
+
+        ratio = pmr(body_parts)
+        if ratio <= 0:
+            continue
+        minimal_prominence = 1 / 4 / ratio
+        maximal_movement = 1 / 8 / ratio
+
+        for body_part in map(BodyPart, range(17)):
+            x, y, c = body_parts[body_part]
+            x_, y_, c_ = body_parts_prev[body_part]
+
+            # if confidence dropped by more than 20% from last frame
+            # and the body part moved more than 12cm, don't move.
+            # this should be dynamic (the less confident the less movement)
+            if c + 0.2 < c_:
+                if abs(x - x_) > maximal_movement:
+                    x = x_
+
+                if abs(y - y_) > maximal_movement:
+                    y = y_
+
+            # smoothing
+            body_parts_prev[body_part] = (
+                x_ * alpha + (1 - alpha) * x,
+                y_ * alpha + (1 - alpha) * y,
+                c_ * alpha + (1 - alpha) * c,
+            )
+
+            if c < MINIMAL_CONFIDENCE:
+                continue
+
+            x, y, c = body_parts_prev[body_part]
+            xmax, xmin = peaks[body_part][0]
+            ymax, ymin = peaks[body_part][1]
+            peaks[body_part] = [
+                [max(x, xmax), min(x, xmin)],
+                [max(y, ymax), min(y, ymin)],
+            ]
+
+            if x > xmin + minimal_prominence:
+                peaks[body_part][0][1] = x
+                counts[body_part][0] += 1
+                print(body_part.name, counts[body_part])
+            if y > ymin + minimal_prominence:
+                peaks[body_part][1][1] = y
+                counts[body_part][1] += 1
+                print(body_part.name, counts[body_part])
+            if x < xmax - minimal_prominence:
+                peaks[body_part][0][0] = x
+                counts[body_part][0] += 1
+                print(body_part.name, counts[body_part])
+            if y < ymax - minimal_prominence:
+                peaks[body_part][1][0] = y
+                counts[body_part][1] += 1
+                print(body_part.name, counts[body_part])
 
     # release the memory
     cap.release()
